10ed8d73668fa28157637c72bdc66867b0c91d79
Yağızhan Burak Yakar <yagizhan49@protonmail.com>
2022-07-20 21:02:23 +0300

    Updated extension

---
+++ Service.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * FOSSBilling
+ *
+ * @copyright FOSSBilling (https://www.fossbilling.org)
+ * @license   Apache-2.0
+ *
+ *
+ * This source file is subject to the Apache-2.0 License that is bundled
+ * with this source code in the file LICENSE
+ */
+
+/**
+ * This extension disables critical system settings.
+ * It is recommended to use this extension only for public demo instances.
+ */
+
+namespace Box\Mod\Demo;
+
+class Service
+{
+    const err = 'This option is disabled for demo instances. Visit <a href="https://fossbilling.org/">fossbilling.org</a> for the installation instructions and start using FOSSBilling today.';
+
+    public function uninstall()
+    {
+         throw new \Box_Exception(self::err);
+    }
+
+    public static function onBeforeAdminStaffUpdate(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeAdminStaffDelete(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeAdminStaffCreate(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+
+    public static function onBeforeAdminStaffPasswordChange(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+
+    public static function onBeforeAdminStaffProfileUpdate(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+
+    public static function onBeforeAdminStaffProfilePasswordChange(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeAdminStaffApiKeyChange(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+
+    public static function onBeforeClientProfileUpdate(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeClientProfilePasswordChange(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeAdminClientDelete(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+
+    public static function onBeforeAdminClientUpdate(\Box_Event $event)
+    {
+        $data = $event->getParameters();
+        if($data['id'] == 1 && $data['email'] != 'client@boxbilling.com') {
+            throw new \Box_Exception(self::err);
+        }
+    }
+
+    public static function onBeforeAdminClientPasswordChange(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeAdminSettingsUpdate(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+    
+    public static function onBeforeAdminDeleteCurrency(\Box_Event $event)
+    {
+        throw new \Box_Exception(self::err);
+    }
+}
